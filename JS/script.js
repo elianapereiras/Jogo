e1e923a72fd0c02394dab77bf5855fa0958a2995
eliana <eliana.pereiras@gmail.com>
2020-12-18 15:54:02 -0300

    criar cobrinha

--- JS/script.js
+++ JS/script.js
@@ -7,6 +7,10 @@ snake[0]={
 	y: 8 * box
 }
 let direction="right";
+let food ={
+	x: Math.floor(Math.random() * 15 + 1) * box,
+	y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG(){
 		context.fillStyle="lightgreen";
@@ -18,6 +22,11 @@ function criarcobrinha(){
 		context.fillStyle="green";
 		context.fillRect(snake[i].x, snake[i].y, box, box);
 	}
+}
+
+function drawFood(){
+	context.fillStyle="red";
+	context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('Keydown', update);
@@ -35,8 +44,10 @@ function iniciarJogo(){
 	if(snake[0].x < 0 && direction == "left") snake[0].x= 16 * box;
 	if(snake[0].y > 15 * box && direction == "down") snake[0].y=0;
 	if(snake[0].y < 0 && direction == "up") snake[0].y=16 * box;
+	
 	criarBG();
 	criarcobrinha();
+	drawFood();
 	
 	let snakeX= snake[0].x;
 	let snakeY= snake[0].y;
