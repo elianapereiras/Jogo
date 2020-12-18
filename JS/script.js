2e59612ff2c167356662a14e1a10aafdadd8a620
eliana <eliana.pereiras@gmail.com>
2020-12-18 15:22:09 -0300

    definir direções e impedir que a cobrinha desapareça

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,60 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box=32;
+let snake =[];
+snake[0]={
+	x: 8 * box,
+	y: 8 * box
+}
+let direction="right";
 
 function criarBG(){
 		context.fillStyle="lightgreen";
 		context.fillRect(0,0,16 * box, 16 * box);
 }
 
-criarBG();
+function criarcobrinha(){
+	for(i=0; i< snake.length; i++){
+		context.fillStyle="green";
+		context.fillRect(snake[i].x, snake[i].y, box, box);
+	}
+}
+
+document.addEventListener('Keydown', update);
+
+function update(event){
+		if(event.KeyCode ==37 && direction != "right")direction ="left";
+		if(event.KeyCode ==38 && direction != "down")direction ="up";
+		if(event.KeyCode ==39 && direction != "left")direction ="right";
+		if(event.KeyCode ==40 && direction != "up")direction ="down";
+}
+
+function iniciarJogo(){
+	
+	if(snake[0].x > 15 * box && direction == "right") snake[0].x=0;
+	if(snake[0].x < 0 && direction == "left") snake[0].x= 16 * box;
+	if(snake[0].y > 15 * box && direction == "down") snake[0].y=0;
+	if(snake[0].y < 0 && direction == "up") snake[0].y=16 * box;
+	criarBG();
+	criarcobrinha();
+	
+	let snakeX= snake[0].x;
+	let snakeY= snake[0].y;
+	
+	if(direction == "right") snakeX += box;
+	if(direction == "rleft") snakeX -= box;
+	if(direction == "up") snakeY -= box;
+	if(direction == "down") snakeY += box;
+	
+	snake.pop();/*remove o último elemento do array*/
+	
+	let newHead= {
+		x: snakeX,
+		y: snakeY
+	}
+	snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo,100); /* tempo em milesegundo*/
+
+
